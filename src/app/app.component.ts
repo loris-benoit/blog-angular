185c720c06984594c2ba634565e5e689adff1e55
loris-benoit <55584492+loris-benoit@users.noreply.github.com>
2019-09-20 13:20:25 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,17 +12,17 @@ export class AppComponent {
   posts = [
     {
       title: 'Premier post',
-      content: 'La nouvelle vidéo est déjà à + de 400k vues en 24h putain vs l’avez tellement partagée que mm l’algorithme YTube a explosé son shadowban et m’a foutu en tendance Jeux vidéo',
+      content: 'Ceci est le contenu de mon 1er post',
       created: this.date
     },
     {
       title: 'Deuxieme post',
-      content: '@Eystein Bye iaverys solution is working just I am trying to reduce the distance between the FAA/ATC NEXT DAY PLAN and the date now it goes in extreme right',
+      content: 'Ceci est le contenu de mon 2nd post',
       created: this.date
     },
     {
       title: 'Troisieme post',
-      content: 'Angular appliquera donc systématiquement la classe  list-group-item , et selon le contenu de la variable  appareilStatus , appliquera lune ou lautre des deux autres classes.  Vous pouvez bien évidemment créer vos propres classes et les utiliser',
+      content: 'Ceci est le contenu de mon dernier post',
       created: this.date
     }
   ];
